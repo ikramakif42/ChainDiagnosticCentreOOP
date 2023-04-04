68ee08641ef97f6ae822b04b4dd0d18b1cd00ec1
Kazi <kazi.s.iqbal@outlook.com>
2023-04-04 21:20:35 +0600

    Director and Accounts Officer Added

---
+++ src/views/director/DirectorDashboardController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package views.director;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kazi
+ */
+public class DirectorDashboardController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void directorEmployeeListOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void directorSchedulesOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void directorPromoteTransferOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void directorFinanceHRReportsOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void directorSalaryOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void directorBranchReportsOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void direcotr(ActionEvent event) {
+    }
+
+    @FXML
+    private void directorPastRecordsOnClick(ActionEvent event) {
+    }
+    
+}

67f5a9a13ffdb7d8900df2b3a3716e5356ab94f1
rakibahmad007 <ahmadrakib43@gmail.com>
2023-04-16 13:06:31 +0600

    new commit
    new commit

---
+++ src/views/nurse/NursePatientBillController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package views.nurse;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class NursePatientBillController implements Initializable {
+
+    @FXML
+    private TableView<?> nursePatientBillTableList;
+    @FXML
+    private Button nursePatientBillFilter;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void nursePatientBillFilterOnClick(ActionEvent event) {
+    }
+    
+}

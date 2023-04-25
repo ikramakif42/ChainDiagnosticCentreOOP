36104f7dbea113db9c0522df4ff7f993ff2c6149
ikramakif42 <ikramakif42@gmail.com>
2023-04-25 21:49:50 +0600

    bill class date

--- src/views/patient/ConfirmCancelApptController.java
+++ src/views/patient/ConfirmCancelApptController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package views.patient;
 
 import java.net.URL;
@@ -10,22 +5,24 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import users.Patient;
 
-/**
- * FXML Controller class
- *
- * @author Asus
- */
 public class ConfirmCancelApptController implements Initializable {
+    private Patient patient;
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
 
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+    
     @FXML
     private void cancelApptOnClick(ActionEvent event) {
     }

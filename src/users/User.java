4324d25755fddd5e9371a52aff2c4947679849c8
MD-ARAFATH <mdarafath2500@gmail.com>
2023-04-29 22:58:11 +0600

    addHR
    AddHR

--- src/users/User.java
+++ src/users/User.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.time.LocalDate;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import model.LoginInfo;
+import model.Policy;
 
 public abstract class User implements Serializable{
     private static final long serialVersionUID = 13L;
@@ -258,4 +261,42 @@ public abstract class User implements Serializable{
         }
         return null;
     }
-}
+
+    public static ObservableList<Policy> getPolicyList() {
+        ObservableList<Policy> PolicyList = FXCollections.observableArrayList();
+        File f = null;
+        FileInputStream fis = null;      
+        ObjectInputStream ois = null;
+        String path = "PolicyObjects.bin";
+        try {
+            f = new File(path);
+            fis = new FileInputStream(f);
+            ois = new ObjectInputStream(fis);
+            Policy tempPolicy = null;
+            try{
+                System.out.println("Printing objects");
+                while(true){
+                    tempPolicy = (Policy) ois.readObject();
+                    System.out.println("Populate Policy:");
+                    System.out.println(tempPolicy.toString());
+                    PolicyList.add((Policy)tempPolicy);
+                }
+            }
+            catch(IOException | ClassNotFoundException e){
+                System.out.println(e.toString());
+                System.out.println("IOException | ClassNotFoundException in reading bin file");
+            }
+            System.out.println("End of file\n");
+        } catch (IOException ex) {
+            System.out.println("IOException on entire file handling");
+        }
+        finally {
+            try {
+                if(ois != null) ois.close();
+            } catch (IOException ex) { }
+        }
+        System.out.println(PolicyList);        
+        return PolicyList;
+    }    
+    }
+
